e8b13906715eed7b458b15453a70610e7157be72
Xophules <Xophules@users.noreply.github.com>
2018-11-12 23:25:08 -0600

    Add files via upload

---
+++ testing/OpenDiscord.sikuli/OpenDiscord.py
@@ -0,0 +1,12 @@
+click("1542085941997.png")
+type("Discord")
+click("1542086105584.png")
+wait(3)
+
+click(Pattern("1542086267278.png").similar(0.90))
+click(Pattern("1542086286036.png").similar(0.85))
+click(Pattern("1542086303643.png").similar(0.85).targetOffset(-160,-3))
+
+
+
+
